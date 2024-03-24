dd2eacd3e624f2c8ffc1d75ddffa94914c02745e
AstralPhoenix01 <cardinalphoenix940@gmail.com>
2024-03-23 22:27:37 -0400

    created function for printing foods and their ingredients

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -25,10 +25,30 @@ def getStationFoods():
             station_menus.append(station_menu)
     return station_menus
 
+def getFoodsAndTheirIngredients():
+    ingredients = []
+    food_list = []
+    for station_element in station_elements:
+        station_data = station_element.find("div")
+        foods = station_data.find_all("li")
+        for food in foods:
+            food_attributes = food.attrs
+            food_name_element = food.find("a")
+            food_name = food_name_element.get_text()
+            food_ingredients = food_attributes["data-searchable"]
+            ingredients.append(food_ingredients[len(food_name):len(food_ingredients)])
+            food_list.append(food_name)
+    return food_list, ingredients
+
 def printStationMenus():
     for i in range(len(station_names)):
         print(station_names[i] + ":")
         print(station_menus[i])
+
+def printFoodsAndIngredients():
+    for i in range(len(food_list)):
+        print(food_list[i] + ":")
+        print(ingredients[i])
 
 url = "https://harvesttableuva.com/locations/runk-dining-hall/"
 page = requests.get(url)
@@ -36,8 +56,11 @@ soup = BeautifulSoup(page.text, 'html.parser')
 station_elements = soup.find_all(attrs={"menu-station"})
 station_names = getStationNames()
 station_menus = getStationFoods()
+food_list, ingredients = getFoodsAndTheirIngredients()
+
 
 printStationMenus()
+printFoodsAndIngredients()
 
 # stations = soup.find_all(attrs={"class":"toggle-menu-station-data"})
 
