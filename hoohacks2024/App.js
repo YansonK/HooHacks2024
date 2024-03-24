ccc79e9e95948011155deb79e2d48f644f013491
lanah.png <dmt9xb@virginia.edu>
2024-03-23 22:03:56 -0400

    loading...

--- hoohacks2024/App.js
+++ hoohacks2024/App.js
@@ -6,23 +6,45 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://172.25.167.129:5000/api/stations') // replace with your Flask API endpoint
-      .then(response => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/stations');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-        return response.json();
-      })
-      .then(data => setStationsData(data))
-      .catch(error => console.error('There has been a problem with your fetch operation: ', error));
+        const data = await response.json();
+        setStationsData(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation: ', error);
+      }
+    };
+  
+    fetchData();
   }, []);
+  
   
 
   const diningHalls = [
-    { name: 'O-Hill', image: 'https://branchbuilds.com/wp-content/uploads/2021/09/observatory-hill-03.jpg', Stations: stationsData[0] },
-    { name: 'FFC', image: 'https://www.coleanddenny.com/wp-content/uploads/2020/07/HDPhoto_130723_07_FS1.jpg', Stations: stationsData[1] },
-    { name: 'Runk', image: 'https://tipton-associates.com/wp-content/uploads/2020/09/RunkDiningHall_Img1.jpg', Stations: stationsData[2] },
+    { 
+      name: 'O-Hill', 
+      image: 'https://branchbuilds.com/wp-content/uploads/2021/09/observatory-hill-03.jpg', 
+      Stations: isLoading ? "loading..." : `${stationsData[0]}` 
+    },
+    { 
+      name: 'FFC', 
+      image: 'https://www.coleanddenny.com/wp-content/uploads/2020/07/HDPhoto_130723_07_FS1.jpg', 
+      Stations: isLoading ? "loading..." : `${stationsData[1]}` 
+    },
+    { 
+      name: 'Runk', 
+      image: 'https://tipton-associates.com/wp-content/uploads/2020/09/RunkDiningHall_Img1.jpg', 
+      Stations: isLoading ? "loading..." : `${stationsData[2]}` 
+    },
   ];
+  
+  
+  
 
   // Get the screen width
   const screenWidth = Dimensions.get('window').width;
@@ -33,15 +55,11 @@ export default function App() {
   // Calculate the width of each dining hall container
   const containerWidth = (screenWidth - 40) / numColumns - 10;
 
-  if (isLoading) {
-    return <Text>Loading...</Text>; 
-  }
-
   return (
     <View style={styles.container}>
       <ImageBackground source={{ uri: 'https://media.licdn.com/dms/image/D4E16AQEhhFL-_sZ_xQ/profile-displaybackgroundimage-shrink_200_800/0/1667594600063?e=2147483647&v=beta&t=bWvcgGTNQOcJBh7nuRQmjOt1YF6N3rHr6aiNxcJpDWo' }} style={styles.topBar}></ImageBackground>
       <View style={styles.chipContainer}>
-        {['Egg', 'Milk', 'Other'].map((chip, index) => (
+        {['X Egg', 'X Milk', 'Other'].map((chip, index) => (
           <TouchableOpacity key={index} style={styles.chip}>
             <Text>{chip}</Text>
           </TouchableOpacity>
@@ -53,7 +71,7 @@ export default function App() {
           <TouchableOpacity key={index} style={[styles.diningHallContainer, { width: containerWidth }]}>
             <Image source={{ uri: diningHall.image }} style={styles.diningHallImage} />
             <Text style={styles.diningHallName}>{diningHall.name}</Text>
-            <Text style={styles.diningHallStations}>{diningHall.Stations} Available Stations</Text>
+            <Text style={styles.diningHallStations}>Available Stations: {diningHall.Stations}</Text>
           </TouchableOpacity>
         ))}
       </ScrollView>
@@ -73,7 +91,6 @@ const styles = StyleSheet.create({
   },
   chipContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     marginVertical: 10,
     marginHorizontal: 20,
   },
@@ -82,6 +99,7 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     borderRadius: 20,
+    marginHorizontal: 10,
   },
   title: {
     fontSize: 24,
@@ -117,7 +135,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: 'gray',
     position: 'absolute',
-    bottom:20,
+    bottom:8,
     left: 10,
   },
 });
